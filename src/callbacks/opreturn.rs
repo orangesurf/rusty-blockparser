d36826ade626ab1ca06d82b861965001ee66fc35
orangesurf <admin@orange.surf>
2025-10-05 16:44:30 +0800

    Initial implementation of OP_RETURN dump to csv

--- src/callbacks/opreturn.rs
+++ src/callbacks/opreturn.rs
@@ -1,12 +1,16 @@
-use clap::{ArgMatches, Command};
+use clap::{ArgMatches, Command, Arg};
+use std::fs::File;
+use std::io::{BufWriter, Write};
+use std::path::PathBuf;
 
 use crate::blockchain::proto::block::Block;
 use crate::blockchain::proto::script::ScriptPattern;
 use crate::callbacks::Callback;
 use crate::common::Result;
 
-#[derive(Default)]
-pub struct OpReturn;
+pub struct OpReturn {
+    writer: BufWriter<File>,
+}
 
 impl Callback for OpReturn {
     fn build_subcommand() -> Command
@@ -14,16 +18,34 @@ impl Callback for OpReturn {
         Self: Sized,
     {
         Command::new("opreturn")
-            .about("Shows embedded OP_RETURN data that is representable as UTF8")
+            .about("Exports OP_RETURN trend data to CSV")
             .version("0.1")
             .author("gcarq <egger.m@protonmail.com>")
+            .arg(
+                Arg::new("output")
+                    .help("Output CSV file path")
+                    .value_name("FILE")
+                    .required(true)
+                    .index(1),
+            )
     }
 
-    fn new(_: &ArgMatches) -> Result<Self>
+    fn new(matches: &ArgMatches) -> Result<Self>
     where
         Self: Sized,
     {
-        Ok(OpReturn)
+        let output_path = PathBuf::from(matches.get_one::<String>("output").unwrap());
+        let file = File::create(output_path)?;
+        // Use smaller 4KB buffer to reduce RAM usage
+        let mut writer = BufWriter::with_capacity(4096, file);
+        
+        // Write CSV header
+        writeln!(
+            writer,
+            "block_height,block_timestamp,txid,data_length,tx_output_index"
+        )?;
+        
+        Ok(OpReturn { writer })
     }
 
     fn on_start(&mut self, _: u64) -> Result<()> {
@@ -32,16 +54,25 @@ impl Callback for OpReturn {
     }
 
     fn on_block(&mut self, block: &Block, block_height: u64) -> Result<()> {
+        let block_timestamp = block.header.value.timestamp;
+        
         for tx in &block.txs {
-            for out in tx.value.outputs.iter() {
+            for (output_index, out) in tx.value.outputs.iter().enumerate() {
                 if let ScriptPattern::OpReturn(data) = &out.script.pattern {
                     if data.is_empty() {
                         continue;
                     }
-                    println!(
-                        "height: {: <9} txid: {}    data: {}",
-                        block_height, &tx.hash, data
-                    );
+                    
+                    // Write CSV row
+                    writeln!(
+                        self.writer,
+                        "{},{},{},{},{}",
+                        block_height,
+                        block_timestamp,
+                        &tx.hash,
+                        data.len(),
+                        output_index
+                    )?;
                 }
             }
         }
@@ -49,6 +80,7 @@ impl Callback for OpReturn {
     }
 
     fn on_complete(&mut self, _: u64) -> Result<()> {
+        self.writer.flush()?;
         Ok(())
     }
 
